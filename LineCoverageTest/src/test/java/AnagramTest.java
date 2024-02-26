0be0aef7a60679dbdf8670ad8de224544e2a1204
oussama <oussamabelouahem66@gmail.com>
2024-02-26 21:19:48 +0100

    commit

---
+++ LineCoverageTest/src/test/java/AnagramTest.java
@@ -0,0 +1,28 @@
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.example.Anagram.isAnagram;
+
+public class AnagramTest {
+    @Test
+    public void isAnagramshouldreturnTrueIfTheInputIsanagram() {
+     Assertions.assertTrue(isAnagram("listen", "silent"));
+    }
+    @Test
+    public void isAnagramshouldreturnFalseIfTheInputIsNotanagram(){
+        Assertions.assertFalse(isAnagram("hello", "world"));
+    }
+    @Test
+    public void isAnagramshouldreturnFalseIfTheInputsAreNotequaleInlenght() {
+        Assertions.assertFalse(isAnagram("hellom", "world"));
+    }
+    @Test
+
+    public void IsPalindrommeShouldReturnExeptionIfTheInputsArenull()
+    {
+        Assertions.assertThrows(NullPointerException.class,() ->isAnagram(null, null));
+    }
+
+}
