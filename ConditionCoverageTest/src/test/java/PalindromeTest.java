e41d7cbfa07e0162f9aea42abae4834fcb7fa666
oussama <oussamabelouahem66@gmail.com>
2024-02-26 20:59:39 +0100

    commit

---
+++ ConditionCoverageTest/src/test/java/PalindromeTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.example.PalindromeCorrection.isPalindrome;
+
+public class PalindromeTest {
+    @Test
+    void NullStringShouldThrowException(){
+        Assertions.assertThrows(NullPointerException.class, ()->isPalindrome(null));
+    }
+    @Test
+    void StringIsPalindrome(){
+        Assertions.assertTrue(isPalindrome("radar"));
+    }
+    @Test
+    void StringIsNotPalindrome(){
+        Assertions.assertFalse(isPalindrome("oussama"));
+    }
+}
