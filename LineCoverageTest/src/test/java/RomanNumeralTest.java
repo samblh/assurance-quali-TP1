6fc7af5f7988b51c781675d983d475e48fb40dbd
oussama <oussamabelouahem66@gmail.com>
2024-02-27 01:15:11 +0100

    commit

--- LineCoverageTest/src/test/java/RomanNumeralTest.java
+++ LineCoverageTest/src/test/java/RomanNumeralTest.java
@@ -9,4 +9,10 @@ public class RomanNumeralTest {
     void TestToRoman(int input, String expectedValue){
         Assertions.assertEquals(expectedValue, toRoman(input));
     }
+    @Test
+    void numberOutOfBoundriesShouldThrowException(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> toRoman(0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> toRoman(4000));
+    }
 }
+
